b8103cb72e82e054a49cd40730534e94f8b41d33
ZOI-dayo <64856587+ZOI-dayo@users.noreply.github.com>
2021-12-15 23:30:02 +0900

    init with intellij

---
+++ src/main/java/net/zoizoi/spigot/jump_and_firework/JumpAndFirework.java
@@ -0,0 +1,17 @@
+package net.zoizoi.spigot.jump_and_firework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JumpAndFirework extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
