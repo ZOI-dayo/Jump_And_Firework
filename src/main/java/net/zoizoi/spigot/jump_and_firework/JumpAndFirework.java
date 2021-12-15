4f1ba1eabdc7f24cc6d86a8840d20a87eb99c802
ZOI-dayo <64856587+ZOI-dayo@users.noreply.github.com>
2021-12-15 23:39:26 +0900

    終わり

--- src/main/java/net/zoizoi/spigot/jump_and_firework/JumpAndFirework.java
+++ src/main/java/net/zoizoi/spigot/jump_and_firework/JumpAndFirework.java
@@ -1,5 +1,14 @@
 package net.zoizoi.spigot.jump_and_firework;
 
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JumpAndFirework extends JavaPlugin {
@@ -13,5 +22,18 @@ public final class JumpAndFirework extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJump(PlayerMoveEvent event) { // 名前はどうでも良い
+        if(event.getFrom().getY() == event.getTo().getY()) return; // 高さ変わってなかったら無視
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+        Firework firework = (Firework) world.spawnEntity(player.getLocation(), EntityType.FIREWORK);
+        FireworkMeta meta = firework.getFireworkMeta();
+        meta.setPower(3);
+        meta.addEffect(FireworkEffect.builder().withColor(Color.LIME).flicker(true).build());
+        firework.setFireworkMeta(meta);
+        // event.getPlayer().getLocation()
     }
 }
